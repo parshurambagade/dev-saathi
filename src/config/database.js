d761db38d15672a7f56c485b2513510eed83c9a2
Parshuram Bagade <parshurambagade855@gmail.com>
2025-07-18 12:33:16 +0530

    chore: clean up root directory after restructuring to client-server architecture
    - Remove backend files from root directory after moving to server folder
    - Delete package.json, package-lock.json, and .env.example from root
    - Remove src/ directory and all backend code (models, routes, middlewares, utils)
    - Clean up database config and constants files from root
    - Update .gitignore to reflect new project structure
    - Complete transition to organized client-server folder structure

--- src/config/database.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import { MONGO_URI } from "../constants.js";
-
-const connectDb = async () => {
-  await mongoose.connect(MONGO_URI);
-};
-
-export default connectDb;
